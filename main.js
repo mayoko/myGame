75e13818c36b9ea18a7f6cb9cd4ad562f902907f
root <root@LAPTOP-0180ADHT.localdomain>
2018-05-18 23:46:32 +0900

    初手の見てくれは悪くない

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ let global = {
   keys: []
 };
 
+let Settings = {
+  CELL_WIDTH: 100,
+  CELL_HEIGHT: 100,
+  CELL_MARGIN: 6
+};
+
 // ゲームの状態を管理するクラス
 class State {
   constructor() {
@@ -22,7 +28,7 @@ class State {
   calcNextState(dir) {
     nextState = new State();
     // コピーする
-    for (int i = 0; i < 16; i++) {
+    for (let i = 0; i < 16; i++) {
       nextState.board[i] = this.board[i];
     }
     if (dir == 0) {
@@ -68,6 +74,21 @@ class State {
       }
     }
     return true;
+  }
+  // 空き領域を配列にして返す
+  getEmptyCells() {
+    for (let i = 0; i < 16; i++) {
+      let result = [];
+      if (this.board[i] == 0) {
+        result.push(i);
+      }
+      return result;
+    }
+  }
+  // セルの値を書き換える(ランダムに出現するのをイメージ)
+  // だけど別の用途で使ってもいいやという
+  rewriteCells(index, value) {
+    this.board[index] = value;
   }
   // board を時計回りに回転させる
   _rotate(rot) {
@@ -138,7 +159,8 @@ class Game {
     // セルの集合を定義
     this.cells = [];
     for (let i = 0; i < 16; i++) {
-      const cell = new Cell();
+      const y = Math.floor(i / 4), x = i % 4;
+      const cell = new Cell(y, x);
       cell.changeAttrib(0);
       this.cells.push(cell);
       this.screen.appendChild(cell.elem);
@@ -185,89 +207,15 @@ class Game {
     // 死んでないかチェック
     this.isOver = this._check();
   }
-  isOver() {
-    return this.isOver;
-  }
-  // ゲームオーバーでないか確かめる
-  // ゲームオーバーなら true, そうでないなら false
-  _check() {
-    // 全部埋まっていて, かつどの隣り合ったセル同士も異なっていたら true
-    for (let i = 0; i < 4; i++) {
-      for (let j = 0; j < 4; j++) {
-        if (this.cells[i*4+j].num === 0) {
-          return false;
-        }
-        const dx = [1, 0];
-        const dy = [0, 1];
-        for (let k = 0; k < 2; ++k) {
-          let ni = i + dy[k], nj = j + dx[k];
-          if (ni >= 4 || nj >= 4) {
-            continue;
-          }
-          if (this.cells[i*4+j].num === this.cells[ni*4+nj].num) {
-            return false;
-          }
-        }
-      }
-    }
-    return true;
-  }
-  // board を時計回りに回転させる
-  _rotate(rot) {
-    for (let i = 0; i < rot; ++i) {
-      let nextCells = [];
-      for (let j = 0; j < 16; ++j) {
-        let y = Math.floor(j/4), x = j - 4*y;
-        let ny = x, nx = 3-y;
-        nextCells[ny*4+nx] = this.cells[j];
-      }
-      this.cells = nextCells;
-    }
-  }
-  // board の上側にセルを動かす
-  // 少なくとも一つのセルが動いたなら true, そうでないなら false
-  _moveUp() {
-    let result = false;
-    // merge 情報をリセット
-    for (let y = 0; y < 4; y++) {
-      for (let x = 0; x < 4; x++) {
-        this.cells[y*4+x].merge = false;
-      }
-    }
-    // 上から順番に見ていき, くっつけられるならくっつけていく
-    for (let y = 1; y < 4; y++) {
-      for (let x = 0; x < 4; ++x) {
-        if (this.cells[y*4+x].num === 0) continue;
-        let cy = y;
-        while (cy >= 1) {
-          const num = this.cells[(cy-1)*4+x].num;
-          if (num === 0) {
-            // 上が空いてるなら普通に動かす
-            result = true;
-            this.cells[(cy-1)*4+x].changeAttrib(this.cells[cy*4+x].num);
-            this.cells[cy*4+x].changeAttrib(0);
-            --cy;
-          } else if (num === this.cells[cy*4+x].num && !this.cells[cy*4+x].merge && !this.cells[(cy-1)*4+x].merge) {
-            // 上と同じ数なら合体させる
-            result = true;
-            this.cells[(cy-1)*4+x].changeAttrib(2*num);
-            this.cells[cy*4+x].changeAttrib(0);
-            this.cells[(cy-1)*4+x].merge = true;
-            --cy;
-          } else {
-            break;
-          }
-        }
-      }
-    }
-    return result;
-  }
 }
 
+// アニメーションを管理するクラス
 // Cell を管理するクラス
 class Cell {
-  constructor() {
+  constructor(y, x) {
     this.elem = this._initElement();
+    this.elem.style.left = `${y * (Settings.CELL_HEIGHT + Settings.CELL_MARGIN) + Settings.CELL_MARGIN}px`;
+    this.elem.style.top = `${x * (Settings.CELL_WIDTH + Settings.CELL_MARGIN) + Settings.CELL_MARGIN}px`;
     this.num = 0;
     this.merge = false;
   }
@@ -333,6 +281,8 @@ class Cell {
   _initElement() {
     const result = document.createElement("div");
     result.classList.add("gameCell");
+    result.style.height = `${Settings.CELL_HEIGHT}px`;
+    result.style.width = `${Settings.CELL_WIDTH}px`;
     return result;
   }
 }
