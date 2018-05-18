c496788aff7b76cf131cfff2eb0b47443e930c9d
root <root@LAPTOP-0180ADHT.localdomain>
2018-05-18 23:02:05 +0900

    State クラスがある程度出来た

--- main.js
+++ main.js
@@ -2,6 +2,134 @@ let global = {
   keys: []
 };
 
+// ゲームの状態を管理するクラス
+class State {
+  constructor() {
+    // 4 * 4 行列で, 各要素にどういう要素が入っているかを管理する
+    this.board = [];
+    // move したときに, 例えば 2 と 2 がくっついたりして merge されたかどうかを管理する
+    this.merge = [];
+    // move したときに, 動いたセルはどこからどこへ行ったのか
+    this.moveCells = [];
+    // move があった際に, どこかの要素が動いたかどうかを判定する
+    this.isMove = false;
+    for (let i = 0; i < 16; i++) {
+      this.board.push(0);
+      this.merge.push(false);
+    }
+  }
+  // 指示された方向に動いた際の次の State を求める
+  calcNextState(dir) {
+    nextState = new State();
+    // コピーする
+    for (int i = 0; i < 16; i++) {
+      nextState.board[i] = this.board[i];
+    }
+    if (dir == 0) {
+      // up
+      nextState._moveUp();
+    } else if (dir == 1) {
+      // right
+      nextState._rotate(3);
+      nextState._moveUp();
+      nextState._rotate(1);
+    } else if (dir == 2) {
+      // down
+      nextState._rotate(2);
+      nextState._moveUp();
+      nextState._rotate(2);
+    } else {
+      // left
+      nextState._rotate(1);
+      nextState._moveUp();
+      nextState._rotate(3);
+    }
+    return nextState;
+  }
+  // 死んでないか確かめる
+  isDie() {
+    // 全部埋まっていて, かつどの隣り合ったセル同士も異なっていたら true
+    for (let i = 0; i < 4; i++) {
+      for (let j = 0; j < 4; j++) {
+        if (this.board[i*4+j] === 0) {
+          return false;
+        }
+        const dx = [1, 0];
+        const dy = [0, 1];
+        for (let k = 0; k < 2; ++k) {
+          let ni = i + dy[k], nj = j + dx[k];
+          if (ni >= 4 || nj >= 4) {
+            continue;
+          }
+          if (this.board[i*4+j] === this.board[ni*4+nj]) {
+            return false;
+          }
+        }
+      }
+    }
+    return true;
+  }
+  // board を時計回りに回転させる
+  _rotate(rot) {
+    for (let i = 0; i < rot; ++i) {
+      let nextBoard = [];
+      let nextMerge = [];
+      for (let j = 0; j < 16; ++j) {
+        let y = Math.floor(j/4), x = j - 4*y;
+        let ny = x, nx = 3-y;
+        nextBoard[ny*4+nx] = this.board[j];
+        nextMerge[ny*4+nx] = this.merge[j];
+      }
+      this.board = nextCells;
+      this.merge = nextMerge;
+      let nextMoveCells = [];
+      for (let i = 0; i < this.moveCells.length; ++i) {
+        const moveCell = this.moveCells[i];
+        let fx = moveCell.fx, fy = moveCell.fy, tx = moveCell.tx, ty = moveCell.ty;
+        nextMoveCells.push({fx: 3-fy, fy: fx, tx: 3-ty, ty: tx});
+      }
+      this.moveCells = nextMoveCells;
+    }
+  }
+  // 後 _moveUp が必要ですね
+  _moveUp() {
+    // merge 情報をリセット
+    for (let y = 0; y < 4; y++) {
+      for (let x = 0; x < 4; x++) {
+        this.merge[y*4+x] = false;
+      }
+    }
+    // 上から順番に見ていき, くっつけられるならくっつけていく
+    for (let y = 1; y < 4; y++) {
+      for (let x = 0; x < 4; ++x) {
+        if (this.board[y*4+x] === 0) continue;
+        let cy = y;
+        while (cy >= 1) {
+          const num = this.board[(cy-1)*4+x];
+          if (num === 0) {
+            // 上が空いてるなら普通に動かす
+            this.board[(cy-1)*4+x] = this.board[cy*4+x];
+            this.board[cy*4+x] = 0;
+            --cy;
+          } else if (num === this.board[cy*4+x] && !this.merge[cy*4+x] && !this.merge[(cy-1)*4+x]) {
+            // 上と同じ数なら合体させる
+            this.board[(cy-1)*4+x] = 2*num;
+            this.board[cy*4+x] = 0;
+            this.merge[(cy-1)*4+x] = true;
+            --cy;
+          } else {
+            break;
+          }
+        }
+        if (cy != y) {
+          // 少なくとも一マス動いているので動き情報を記録
+          this.moveCells.push({fx: x, fy: y, tx: x, ty: cy});
+        }
+      }
+    }
+  }
+}
+
 // Game 全体を管理するクラス
 class Game {
   constructor() {
